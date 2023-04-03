715319e12c76658dd8161c18c431b55b284e32ae
lombaleo <129803662+lombaleo@users.noreply.github.com>
2023-04-03 20:13:31 -0300

    Add files via upload

---
+++ atividade 3/atividade3.py
@@ -0,0 +1,11 @@
+
+numero1 = float (input("Digite um número: "))
+numero2 = float (input("Digite outro número: "))
+
+
+if numero1 == numero2:
+    print ("Os número são iguais")
+elif numero1 > numero2:
+    print ("O maior número é ",numero1)
+elif numero2 > numero1:
+    print ("O maior número é ",numero2)
