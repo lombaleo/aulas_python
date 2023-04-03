2987dd5582e48de6d9931c729bc497390ab4445e
lombaleo <129803662+lombaleo@users.noreply.github.com>
2023-04-03 20:27:01 -0300

    Add files via upload

---
+++ atividade 1/atividade.py
@@ -0,0 +1,11 @@
+nome = "leonardo"
+sobrenome = "lomba"
+idade = 2023-2002
+altura = 1.79
+peso = 90
+
+print ("nome: " + nome + " "+ sobrenome + ", idade: "+ str(idade) +" anos, altura: "+ str(altura) +"cm, peso: "+ str(peso) +" Kg")
+if idade > 17:
+        print ("maior de idade")
+else:
+    print ("menor de idade")
